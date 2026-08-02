c9a937fd62b6964f468853607db9330599066f69
JCYaya <jocyaya@gmail.com>
2026-08-02 15:22:42 -0700

    uibuild

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('entry', function getEntry(req) {
     return JSON.stringify(req.body)
